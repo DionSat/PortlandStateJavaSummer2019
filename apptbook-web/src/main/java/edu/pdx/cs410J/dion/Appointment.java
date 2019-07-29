c4ee0d38726b72e642c28fe040e24da44b73d3d8
DionSat <43707506+DionSat@users.noreply.github.com>
2019-07-29 15:19:28 -0700

    started project 4

---
+++ apptbook-web/src/main/java/edu/pdx/cs410J/dion/Appointment.java
@@ -0,0 +1,146 @@
+package edu.pdx.cs410J.dion;
+
+import edu.pdx.cs410J.AbstractAppointment;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Comparator;
+
+/**
+ * This class will create an appointment and store each component the appointment
+ * It is a child of the AbstractAppointment Class
+ */
+public class Appointment extends AbstractAppointment implements Comparable<Appointment>{
+    private Date beginDate;
+    private Date endDate;
+    private String description;
+
+    /**
+     * Function to instantiate the Appointment class
+     * @param newDescription
+     *        the description passed in
+     * @param beginDate
+     *        the beginning time passed in
+     * @param endDate
+     *        the ending time passed in
+     */
+    public Appointment(String newDescription, String beginDate, String endDate) {
+        this.description = newDescription;
+
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyy hh:mm a");
+        try {
+            this.beginDate = df.parse(beginDate.trim());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.endDate = df.parse(endDate.trim());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Function instantiates the Appointment class using date arguments
+     * @param newDescription
+     *        the description passed in
+     * @param beginDate
+     *        the beginning time passed in as a date
+     * @param endDate
+     *        the ending time passed in as a date
+     */
+    public Appointment(String newDescription, Date beginDate, Date endDate) {
+        this.description = newDescription;
+        this.beginDate = beginDate;
+        //this.beginDay = newBeginDay;
+        this.endDate = endDate;
+        //this.endDay = newEndDay;
+    }
+
+    /**
+     * This function concatenates beginning time and the beginDay(am/pm) and returns the resultant string
+     * @return
+     */
+    @Override
+    public String getBeginTimeString() {
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyy hh:mm a");
+        return df.format(beginDate);
+    }
+
+    /**
+     * This function concatenates ending time and the endDay(am/pm) and returns the resultant string
+     * @return
+     */
+    @Override
+    public String getEndTimeString() {
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyy hh:mm a");
+        return df.format(endDate);
+    }
+
+    /**
+     * This function gets the description of the appointment
+     * @return
+     */
+    @Override
+    public String getDescription() {
+        //return "This method is not implemented yet";
+        return description;
+    }
+
+    /**
+     * This function returns the start time of data type date
+     * @return start time
+     */
+    @Override
+    public  Date getEndTime() {
+        return endDate;
+    }
+
+    /**
+     * This function returns the end time of data type date
+     * @return end time
+     */
+    @Override
+    public Date getBeginTime() {
+        return beginDate;
+    }
+
+    /**
+     * Overridden function of compareTo so when objects of Appointment are compared
+     * and checked against each other according to time.
+     * @param o
+     *        The left assignment object compared against.
+     * @return return 1 or 0
+     */
+    @Override
+    public int compareTo(Appointment o) {
+        try {
+            if (this.beginDate == null) {
+                throw new NullPointerException("time and date empty!");
+            }
+            if (this.endDate == null) {
+                throw new NullPointerException("time and date empty");
+            }
+            long difference = this.beginDate.getTime() - o.endDate.getTime();
+
+            if (difference > 0) {
+                return 1;
+            }
+
+            if (difference < 0) {
+                return -1;
+            }
+
+            //they are equal
+            if (difference == 0 ) {
+                return this.description.compareTo(o.description);
+            }
+
+        } catch (NullPointerException e) {
+            System.out.println("Found a Null Pointer in compareTo");
+        }
+        return 0;
+    }
+
+}
---
+++ apptbook-web/src/main/java/edu/pdx/cs410J/dion/AppointmentBook.java
@@ -0,0 +1,59 @@
+package edu.pdx.cs410J.dion;
+
+import edu.pdx.cs410J.AbstractAppointment;
+import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * This class will create Collection of appointment books and store each appointment
+ * It is a child of the AbstractAppointmentBook Class
+ */
+public class AppointmentBook extends AbstractAppointmentBook {
+    private String owner;
+    private Collection<Appointment> appointments = new ArrayList<>();
+
+    /**
+     * Function to instantiate the AppointmentBook class
+     * @param owner
+     *          The owner of the appointment
+     */
+    public AppointmentBook(String owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * Function to return the owner of the appointment
+     * @return return the owner string
+     */
+    @Override
+    public String getOwnerName() {
+        return owner;
+    }
+
+    public void setOwnerName(String owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * Function to return the appointment
+     * @return returns appointment object
+     */
+    @Override
+    public Collection getAppointments() {
+        return appointments;
+    }
+
+    /**
+     * This function adds an appointment to the appointmentbook
+     * @param newAppointment
+     *          The new appointment to be added to the book
+     */
+    @Override
+    public void addAppointment(AbstractAppointment newAppointment) {
+        appointments.add((Appointment) newAppointment);
+    }
+
+    private void sortAppointmentBook(Collection<Appointment> appointments) {}
+}
